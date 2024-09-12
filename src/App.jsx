0b63a5b9388b34932f07dd6eae53861c4c9d16b8
tomas <tomy2002.tf@gmail.com>
2024-09-12 11:36:50 -0300

    Agregamos una funcion para determinar el ganador

--- src/App.jsx
+++ src/App.jsx
@@ -21,6 +21,17 @@ const Square = ({ children, updateBoard, isSelected, index }) => {
   )
 }
 
+// Combinaciones ganadoras (no es optima)
+const WINNER_COMBO = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [5, 6, 7],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
 
 function App() {
   const reset = Array(9).fill(null) // Funcion para resetear el tablero
@@ -29,17 +40,41 @@ function App() {
 
   const [turn, setTurn] = useState(TURNS.X)
 
+  // Estado para saber el ganador
+  const [winner, setWinner] = useState(null) // null => no hay ganador, false => empate 
+
+  // Funcion para determinar el ganador
+  const checkWinner = (boardToCheck) => {
+    // Revisamos todas las combinaciones ganadoras
+    for (const combo of WINNER_COMBO) {
+      const [a, b, c] = combo
+      if (boardToCheck[a] && boardToCheck[a] === boardToCheck[b] && boardToCheck[a] === boardToCheck[c]) {
+        return boardToCheck[a]
+      }
+    }
+
+    // Si no hay ganador 
+    return null
+  }
+
   const updateBoard = (index) => {
-    if (board[index]) return // Si el tablero tiene algo no poner nada
+    // Si el tablero tiene algo o hay un ganador no poner nada
+    if (board[index] || winner) return
 
     // Funcion para cambiar el turno
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
 
-    // Funcion para mostrar la ficha en el tablero
+    // Funcion para actualizar el tablero
     const newBoard = [...board]
     newBoard[index] = turn
     setBoard(newBoard)
+
+    const newWinner = checkWinner(newBoard)
+
+    if (newWinner) {
+      setWinner(newWinner)
+    }
   }
 
   // Funcion para el boton reset
